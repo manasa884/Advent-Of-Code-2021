44c106ab38d1bd184a8c752ef5d3f70d75a6d37e
Manasa Tipparam <mtipparam@Manasas-MacBook-Pro.local>
2021-12-06 23:56:03 -0800

    day 4 pt2

--- day4/play_bingo.py
+++ day4/play_bingo.py
@@ -6,7 +6,9 @@ pp = pprint.PrettyPrinter(indent=2)
 # Part 1
 def find_winner(numbers, boards):
     map = {}
+    winner_list = []
     for num in numbers:
+        # print('Calling number {}'.format(num))
         for b_idx, board in enumerate(boards):
             for r_idx, row in enumerate(board):
                 if num in row:
@@ -17,41 +19,43 @@ def find_winner(numbers, boards):
                         map[str(b_idx)] = [num]
                     else:
                         map[str(b_idx)].append(num)
-        winner = check_for_winners(map, boards)
-        if winner != None:
-            print('Winner: Board #{}'.format(winner))
-            pp.pprint(boards[int(winner)])
-            print('Winning score: {}'.format(
-                get_winning_board_score(boards[int(winner)], num)))
-            break
+        winners = check_for_winners(map, boards)
+        if len(winners) > 0:
+            for w in winners:
+                if w not in winner_list:
+                    winner_list.append(w)
+                    print('Winner: Board #{}'.format(w))
+                    pp.pprint(boards[int(w)])
+                    print('Winning score: {}, Last called number: {}'.format(
+                        get_winning_board_score(boards[int(w)], num), num))
+                    print('--------------------')
 
 
 # Check all boards to see if there are any winners
 def check_for_winners(map, boards):
+    winners = []
     for board_num in map:
         board = boards[int(board_num)]
-        if len(map[board_num]) > 4:
-            for row in board:
-                if row.count('*') == 5:
-                    return board_num
-            for col in range(5):
-                if board[0][col] == '*' and board[1][col] == '*' and board[2][col] == '*' and board[3][col] == '*' and board[4][col] == '*':
-                    return board_num
-    return None
+        # if len(map[board_num]) > 4:
+        for row in board:
+            if row.count('*') == 5:
+                winners.append(board_num)
+                # return board_num
+        for col in range(5):
+            if board[0][col] == '*' and board[1][col] == '*' and board[2][col] == '*' and board[3][col] == '*' and board[4][col] == '*':
+                winners.append(board_num)
+                # return board_num
+    return winners
 
 
 # Calculates final score
-def get_winning_board_score(board, num):
+def get_winning_board_score(board, multiplier):
     sum = 0
     for row in board:
         for num in row:
             if num.isnumeric():
                 sum += int(num)
-    return sum * int(num)
-
-
-# Part 2
-# def get_position_with_aim(lines):
+    return sum * int(multiplier)
 
 
 # Main
